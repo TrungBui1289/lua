906d82bd6e1fbf86f0d97b355d6e3396ddd79f1d
TrungBui1289 <TrungBui1289@users.noreply.github.com>
2022-03-01 15:19:19 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,11 +3,11 @@ local games = "https://raw.githubusercontent.com/TrungBui1289/lua/main/games/"
 local link = {
     [8357510970] = games.."AnimePunchingSimulator.lua",
     [7560156054] = games.."Clicker%20Simulator.lua" ,
-    [7560156054] = games.."AnimeLiftingSimulator.lua" ,
+    [8302406789] = games.."AnimeLiftingSimulator.lua" ,
     [7363858705] = games.."FishSim.lua",
-    [7363858705] = games.."BaseBattles.lua",
+    [5326405001] = games.."BaseBattles.lua",
     [8540346411] = games.."RebirthChampionsX.lua",
-    [7107498084] = games.."UndeadDefenseTycoon.lua"
+    [5670292785] = games.."UndeadDefenseTycoon.lua"
 }
 
 for i, v in pairs(link) do
