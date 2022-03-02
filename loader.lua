493c140c69086d928ab59e28ae6f3b074ccbec7b
TrungBui1289 <TrungBui1289@users.noreply.github.com>
2022-03-02 23:58:01 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,9 +2,7 @@ local games = "https://raw.githubusercontent.com/TrungBui1289/lua/main/games/"
 
 local link = {
     [8357510970] = games.."AnimePunchingSimulator.lua",
-    [7560156054] = games.."ClickerSimulator.lua" ,
     [8302406789] = games.."AnimeLiftingSimulator.lua" ,
-    [7363858705] = games.."FishSim.lua",
     [5326405001] = games.."BaseBattles.lua",
     [8540346411] = games.."RebirthChampionX.lua",
     [5055081323] = games.."SwordmanSimulator.lua",
