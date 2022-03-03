c41a3de415ba09af898f9c0c93f04c3fc9e1b450
TrungBui1289 <TrungBui1289@users.noreply.github.com>
2022-03-04 00:31:20 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,7 +6,8 @@ local link = {
     [5326405001] = games.."BaseBattles.lua",
     [8540346411] = games.."RebirthChampionX.lua",
     [5055081323] = games.."SwordmanSimulator.lua",
-    [5670292785] = games.."UndeadDefenseTycoon.lua"
+    [5670292785] = games.."UndeadDefenseTycoon.lua",
+    [6875469709] = games.."StrongestPunchSim.lua"
 }
 
 for i, v in pairs(link) do
