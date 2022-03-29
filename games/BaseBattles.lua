8304543658a41781a4ab18d21d3ad1f236a0c68a
TrungBui1289 <TrungBui1289@users.noreply.github.com>
2022-03-30 01:56:37 +0700

    Update BaseBattles.lua

--- games/BaseBattles.lua
+++ games/BaseBattles.lua
@@ -54,29 +54,26 @@ Battle:Button(
     "HitBoxes", "",
     function()
 	
-	while (true) do
-	    	wait(0.1)	
-		-- loop
-		getgenv().HeadSize = hitboxAmount
-		getgenv().Disabled = true
-		game:GetService("RunService").Stepped:Connect(function()
-		    -- gets all players in the server
-		    for _, player in next, game:GetService("Players"):GetPlayers() do
-			-- checks if the player found was not the local player, so the local player doesnt get his hitbox extended
-			if player ~= game:GetService("Players").LocalPlayer then
-			    -- finds humanoid root part, then changes transparecy and can collide so you can walk through the hitbox and it wont be wonky
-			    if player.Character:FindFirstChild("HumanoidRootPart") then
-				player.Character["HumanoidRootPart"].CanCollide = false
-				player.Character["HumanoidRootPart"].Transparency = 0.5
-			    end		
-			    -- changes the humanoidrootpart size (basically the main code)
-			    if player.Character["HumanoidRootPart"].Size ~= Vector3.new(getgenv().HeadSize, getgenv().HeadSize, getgenv().HeadSize) then
-				player.Character["HumanoidRootPart"].Size = Vector3.new(getgenv().HeadSize, getgenv().HeadSize, getgenv().HeadSize)
-			    end
-			end
+	-- loop
+	getgenv().HeadSize = hitboxAmount
+	getgenv().Disabled = true
+	game:GetService("RunService").Stepped:Connect(function()
+	    -- gets all players in the server
+	    for _, player in next, game:GetService("Players"):GetPlayers() do
+		-- checks if the player found was not the local player, so the local player doesnt get his hitbox extended
+		if player ~= game:GetService("Players").LocalPlayer then
+		    -- finds humanoid root part, then changes transparecy and can collide so you can walk through the hitbox and it wont be wonky
+		    if player.Character:FindFirstChild("HumanoidRootPart") then
+			player.Character["HumanoidRootPart"].CanCollide = false
+			player.Character["HumanoidRootPart"].Transparency = 0.5
+		    end		
+		    -- changes the humanoidrootpart size (basically the main code)
+		    if player.Character["HumanoidRootPart"].Size ~= Vector3.new(getgenv().HeadSize, getgenv().HeadSize, getgenv().HeadSize) then
+			player.Character["HumanoidRootPart"].Size = Vector3.new(getgenv().HeadSize, getgenv().HeadSize, getgenv().HeadSize)
 		    end
-		end)
-	end
+		end
+	    end
+	end)
     end
 )
 
