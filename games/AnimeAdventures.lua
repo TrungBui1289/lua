2a525f0941a88a47aa488a55046d3bd67204db84
TrungBui1289 <TrungBui1289@users.noreply.github.com>
2022-08-10 02:46:27 +0700

    Update AnimeAdventures.lua

--- games/AnimeAdventures.lua
+++ games/AnimeAdventures.lua
@@ -74,12 +74,12 @@ local function webhook()
 							["name"] = "Area Description:",
 							["value"] = "`" .. areaDescription ..
 								"`",
-							["inline"] = true
+							["inline"] = false
 						},{
 							["name"] = "Area Difficulty:",
 							["value"] = "`" .. difficulty ..
 								"`",
-							["inline"] = false
+							["inline"] = true
 						},{
 							["name"] = "Total Waves:",
 							["value"] = tostring(waves[2]) ..
